132ec1ab44185fec4bf0ff5f8a90cd971706ff55
Uwnag2 <126768997+jeonmye@users.noreply.github.com>
2024-07-02 01:07:05 +0900

    Style: footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-
 import Nav from './Nav';
+import Footer from './Footer';
 
 function Layout() {
   return (
     <div>
       <Container maxWidth="lg">
         <Nav />
-
         <Box sx={{ my: 4 }}>
           <main>
             <Outlet />
           </main>
-          <footer>푸터</footer>
         </Box>
+        <Footer />
       </Container>
     </div>
   );
